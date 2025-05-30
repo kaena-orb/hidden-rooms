64b901dcdf88d9f6ffd55e6b9965b5fd954e9a44
Hernan <pradoh97@gmail.com>
2025-05-30 17:45:08 -0300

    Refactor outside simulated light

--- house.gd
+++ house.gd
@@ -3,39 +3,86 @@ extends Node2D
 signal entered_room
 signal exited_room
 
+var light_tweens : Dictionary
+var canvas_final_color : Color
+var canvas_initial_color : Color = Color.WHITE
+
 func _ready():
-	%PreviewCanvas.visible = false
-	turn_off_lights()
+	canvas_final_color = %DummyCanvas.color
+	%DummyCanvas.color = canvas_initial_color
+	#turn_off_lights(true)
 	%Outside.visible = true
 	%room_area.visible = true
+	$DummyBackground.visible = false
 
 
 func show_room():
+	$Outside/Lights/Sun.range_item_cull_mask= $Outside/Lights/Sun.range_item_cull_mask | ( 1<< 19)
+	for tween in light_tweens:
+		light_tweens[tween].stop()
+		light_tweens[tween].kill()
 	var tween = create_tween()
-	turn_on_lights()
+	tween.set_parallel(true)
+	turn_on_lights(true)
+	tween.tween_property(%DummyCanvas, "color", canvas_final_color, 0.5)
 	tween.tween_property(%Outside, "modulate", Color("#ffffff00"), 0.5)
-	entered_room.emit()
+	light_tweens.set(tween.get_instance_id(), tween)
+	entered_room.emit(%DummyCanvas.color)
+	%DummyCanvas.visible = true
 
 func hide_room():
+	#$Outside/Lights/Sun.range_item_cull_mask & (~int(pow(2, 19)))
+	$Outside/Lights/Sun.range_item_cull_mask = $Outside/Lights/Sun.range_item_cull_mask & ~(1 << 19)
+	#$Outside/Lights/Sun.range_item_cull_mask = 2 <<
+	var tween_duration = 0.5
+	for tween in light_tweens:
+		light_tweens[tween].stop()
+		light_tweens[tween].kill()
 	var tween = create_tween()
-	turn_off_lights()
-	tween.tween_property(%Outside, "modulate", Color("#ffffff"), 0.5)
+	tween.set_parallel(true)
+	tween.tween_property(%DummyCanvas, "color", canvas_initial_color, tween_duration)
+	tween.tween_property(%Outside, "modulate", Color("#ffffff"), tween_duration)
+	light_tweens.set(tween.get_instance_id(), tween)
 	exited_room.emit()
 
-func turn_off_lights():
+	tween.finished.connect(func(): %DummyCanvas.visible = false)
+
+func turn_off_lights(immediate : bool = false):
+	var tween_duration := 2.0
+	if immediate:
+		tween_duration = 0
+
 	for light in %Lights.get_children():
 		if light is Light2D:
 			var tween = create_tween()
-			tween.tween_property(light, "color", Color(light.color, 0), 2)
+			light_tweens.set(tween.get_instance_id(), tween)
+			tween.tween_property(light, "color", Color(light.color, 0), tween_duration)
+			tween.finished.connect(func(): light.enabled = false)
+	for light in $Outside/Lights.get_children():
+		if light is Light2D:
+			var tween = create_tween()
+			light_tweens.set(tween.get_instance_id(), tween)
+			tween.tween_property(light, "color", Color(light.color, 0), tween_duration)
 			tween.finished.connect(func(): light.enabled = false)
 
-func turn_on_lights():
+func turn_on_lights(immediate : bool = false):
+	var tween_duration := 2.0
+	if immediate:
+		tween_duration = 0
+	for light in $Outside/Lights.get_children():
+		if light is Light2D:
+			var tween = create_tween()
+			light_tweens.set(tween.get_instance_id(), tween)
+			light.enabled = true
+			light.color = Color(light.color, 0)
+			tween.tween_property(light, "color", Color(light.color, 1), tween_duration)
 	for light in %Lights.get_children():
 		if light is Light2D:
 			var tween = create_tween()
+			light_tweens.set(tween.get_instance_id(), tween)
 			light.enabled = true
 			light.color = Color(light.color, 0)
-			tween.tween_property(light, "color", Color(light.color, 1), 2)
+			tween.tween_property(light, "color", Color(light.color, 1), tween_duration)
 
 func _on_room_area_area_entered(_area):
 	show_room()
--- level.gd
+++ level.gd
@@ -1,17 +1,17 @@
 extends Node2D
 
 
-func _on_house_entered_room():
-	var tween = create_tween()
-	tween.tween_property(%CanvasModulate, "color", Color("#555555"), 0.5)
+func _on_house_entered_room(color :  Color = Color("#555555")):
+	#var tween = create_tween()
+	#tween.tween_property(%CanvasModulate, "color", color, 0.5)
 	pass
 	#tween.tween_property($DirectionalLight2D, "energy", 0.7, 0.5)
 	#$DirectionalLight2D.enabled = true
 
 
 func _on_house_exited_room():
-	var tween = create_tween()
-	tween.tween_property(%CanvasModulate, "color", Color("#ffffff"), 0.5)
+	#var tween = create_tween()
+	#tween.tween_property(%CanvasModulate, "color", Color("#ffffff"), 0.5)
 	pass
 	#var tween = create_tween()
 	#tween.tween_property($DirectionalLight2D, "energy", 0.0, 0.5)
